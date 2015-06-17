827b85df239d78d905cc92d46f15bb1b9dbb8e22
房文同 <fangwentong2012@gmail.com>
2015-06-17 18:01:31 +0800

    add error prompt

--- src/youdao.py
+++ src/youdao.py
@@ -7,6 +7,14 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+errorInfo = {
+    '20': u'要翻译的文本过长',
+    '30': u'无法进行有效的翻译',
+    '40': u'不支持的语言类型',
+    '50': u'无效的key',
+    '60': u'无词典结果，仅在获取词典结果生效'
+}
+
 def translate(wf):
 
     url = 'http://fanyi.youdao.com/openapi.do'
@@ -16,20 +24,20 @@ def translate(wf):
         type = 'data',
         doctype = 'json',
         version = '1.1',
-        # q = wf.args[0]
-        q = 'hello'
+        q = wf.args[0]
     )
 
     res = web.get(url, params).json()
 
-    if res.get('errorCode', '') == 0:
+    error_code = res.get('errorCode', '')
+    if error_code == 0:
         wf.add_item(res['translation'][0], res.get('query'))
         for item in res['basic']['explains']:
             wf.add_item(item, res['query'])
         for item in res['web']:
             wf.add_item(', '.join(item['value']), item['key'])
     else:
-        wf.add_item(u'没有找到', u'并没有找到你要的单词')
+        wf.add_item(errorInfo[str(error_code)], wf.args[0])
 
     wf.send_feedback()
 
