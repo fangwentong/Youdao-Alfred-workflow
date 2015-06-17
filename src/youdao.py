45299e81887b91360225b04243b6dd94ef06c315
房文同 <fangwentong2012@gmail.com>
2015-06-17 18:14:16 +0800

    fix bug

--- src/youdao.py
+++ src/youdao.py
@@ -2,7 +2,7 @@
 #coding=utf-8
 
 from workflow import Workflow, web
-import sys
+import sys, urllib2
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -20,26 +20,60 @@ def translate(wf):
     url = 'http://fanyi.youdao.com/openapi.do'
     params = dict(
         keyfrom = 'Wentong',
-        key = '1394860876',
-        type = 'data',
+        key     = '1394860876',
+        type    = 'data',
         doctype = 'json',
         version = '1.1',
-        q = wf.args[0]
+        q       = wf.args[0]
     )
 
-    res = web.get(url, params).json()
+    try:
+        res = web.get(url, params).json()
+        error_code = res.get('errorCode', '')
+        if error_code == 0:
+            wf.add_item(
+                title    = res['translation'][0],
+                subtitle = res.get('query'),
+                copytext = res['translation'][0],
+                arg      = res['translation'][0],
+                valid    = True,
+            )
+            if res.get('basic'):
+                for item in res['basic']['explains']:
+                    wf.add_item(
+                        title    = item,
+                        subtitle = res['query'],
+                        copytext = item,
+                        arg      = item,
+                        valid    = True,
+                    )
+            if res.get('web'):
+                for item in res['web']:
+                    wf.add_item(
+                        title    = ', '.join(item['value']),
+                        subtitle = item['key'],
+                        copytext = ', '.join(item['value']),
+                        arg      = ', '.join(item['value']),
+                        valid    = True,
+                    )
+        else:
+            wf.add_item(
+                title    = errorInfo[str(error_code)],
+                subtitle = wf.args[0],
+            )
 
-    error_code = res.get('errorCode', '')
-    if error_code == 0:
-        wf.add_item(res['translation'][0], res.get('query'))
-        for item in res['basic']['explains']:
-            wf.add_item(item, res['query'])
-        for item in res['web']:
-            wf.add_item(', '.join(item['value']), item['key'])
-    else:
-        wf.add_item(errorInfo[str(error_code)], wf.args[0])
-
-    wf.send_feedback()
+    except urllib2.URLError:
+        wf.add_item(
+            title    = u'网络异常',
+            subtitle = u'请检查网络设置',
+        )
+    except:
+        wf.add_item(
+            title    = u'未知错误',
+            subtitle = u'未知错误',
+        )
+    finally:
+        wf.send_feedback()
 
 if __name__ == '__main__':
     wf = Workflow()
